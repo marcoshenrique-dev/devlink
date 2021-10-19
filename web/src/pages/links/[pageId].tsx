1d739404799e134887a5d8eb9186c33706a24b7f
Marcos Henrique <marcoshenriqueh393@gmail.com>
2021-10-19 08:53:03 -0300

    feat: criando página de links

--- web/src/pages/links/[pageId].tsx
+++ web/src/pages/links/[pageId].tsx
@@ -1,11 +1,73 @@
+import { AxiosResponse } from 'axios';
 import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import toast from 'react-hot-toast';
+import api from '../../services/api';
+
+type Page = {
+  id: string;
+  links: Array<string>;
+  url: string;
+  userId: string;
+}
 
 function Links() {
-
+  
   const { query } = useRouter();
+  const {pageId} = query;
+
+  const notify = () => toast('Algo deu errado ao buscar a página');
+
+  const [page, setPage] = useState<Page | null>(null);
+
+  async function findPage() {
+   try {
+    const result: AxiosResponse<Page> = await api.get(`/pages/${pageId}`);
+
+    if(result.data === null) {
+      notify();
+    }
+
+    setPage(result.data);
+
+    console.log(result.data);
+   }
+   catch{
+    notify();
+   }
+  }
+
+  useEffect(() => { 
+    if(pageId !== undefined){
+      findPage();
+    }
+  }, [pageId]);
 
   return(
-    <h2>{query.pageId}</h2>
+    <main className="w-screen h-screen flex flex-col items-center justify-center">
+    <h2 className="font-semibold text-2xl mb-10">{pageId}</h2>
+
+    <section >
+      {
+        page === null ? (
+          <p>Links não encontrados</p>
+        ) : (
+          <div>
+            {
+              page.links.map((item, index) => {
+                return (
+                  <p className="text-gray-500 flex items-center justify-center hover:text-blue-500 mb-5">
+                    <a target="_blank" href={item}>{item}</a>
+                  </p>
+                );
+              })
+            }
+          </div>
+        )
+      }
+    </section>
+
+    </main>
   );
 }
 
